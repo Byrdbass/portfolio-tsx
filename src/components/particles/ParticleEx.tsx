5c534a664502cf7da7b32cab3333999371323baa
byrdbassGit <byrdbass@gmail.com>
2025-04-01 09:47:54 -0500

    finally got it to start working, but confused as to why i still need the useEffect to work, If i comment out the useEffect it stops working

--- src/components/particles/ParticleEx.tsx
+++ src/components/particles/ParticleEx.tsx
@@ -1,19 +1,24 @@
 import { useCallback, useState, useEffect } from "react";
-import  Particles, {initParticlesEngine}  from "@tsparticles/react";
+import  { Particles, initParticlesEngine}  from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-// import { loadFull } from "tsparticles";
-// import { Engine } from "@tsparticles/engine";
+import { loadFull } from "tsparticles";
+import { Engine } from "@tsparticles/engine";
 import './particlesEx.css'
 
 
 const ParticleEx = () => {
     const[init, setInit] = useState(false);
 
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log("Particles Init");
+        await loadFull(engine);
+    }, []);
+
     useEffect(() => {
         const initEngine = async () => {
-          await initParticlesEngine(async (engine) => {
+          await initParticlesEngine(async (engine: Engine) => {
             // Use loadSlim for basic features or loadFull for all features
-            await loadSlim(engine);
+            await loadFull(engine);
             // or await loadFull(engine);
             console.log("initializing particles");
           });
@@ -28,11 +33,33 @@ const ParticleEx = () => {
     return (
         <>
         <h1>Hello</h1>
-        {init && (
+        {/* useCallback version */}
+        <Particles
+                id="tsparticles"
+                init={particlesInit}
+                options={{
+                    preset: "snow",
+                    particles: {
+                        number: {
+                            value: 100,
+                        },
+                        color: {
+                            value: "#ff0000",
+                        },
+                        move: {
+                            enable: true,
+                            speed: 5,
+                        },
+                    },
+                }}
+            />
+
+        {/* useState version */}
+        {/* {init && (
             <Particles
             id="tsparticles"
             options={{
-              preset: "snow",
+              preset: "default",
               particles: {
                 color: {
                   value: "#ff0000"
@@ -40,7 +67,7 @@ const ParticleEx = () => {
               }
             }}
           />
-        )}
+        )} */}
       </>
     );
 }
