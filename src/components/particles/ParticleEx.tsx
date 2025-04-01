553fac90d9421e3d878f388c6e25354f26335dd5
byrdbassGit <byrdbass@gmail.com>
2025-04-01 09:50:02 -0500

    still working on refresh, and commented out useState variable

--- src/components/particles/ParticleEx.tsx
+++ src/components/particles/ParticleEx.tsx
@@ -7,7 +7,7 @@ import './particlesEx.css'
 
 
 const ParticleEx = () => {
-    const[init, setInit] = useState(false);
+    // const[init, setInit] = useState(false);
 
     const particlesInit = useCallback(async (engine: Engine) => {
         console.log("Particles Init");
@@ -23,7 +23,7 @@ const ParticleEx = () => {
             console.log("initializing particles");
           });
           
-          setInit(true);
+        //   setInit(true);
         };
         
         initEngine();
