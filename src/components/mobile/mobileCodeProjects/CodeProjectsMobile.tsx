0ee2da4525b28d7f0d70f8da8924dd7e0ddc402e
byrdbassGit <byrdbass@gmail.com>
2025-04-10 15:19:48 -0500

    ran into problem wrapping a tag for github link around image - need to reconfigure to have this render components all the way through

--- src/components/mobile/mobileCodeProjects/CodeProjectsMobile.tsx
+++ src/components/mobile/mobileCodeProjects/CodeProjectsMobile.tsx
@@ -5,6 +5,7 @@ import './codeProjectsMobile.css'
 import ParticlesContext from "../../../Providers/ParticlesProvider/ParticlesContext";
 import { useDesktopMode } from "../../../Providers/Desktop/DesktopProvider";
 import MobileHomePage from "../mobileHomePage/MobileHomePage";
+import MobileInstructions from "../mobileInstructions/MobileInstructions";
 
 interface ImageData {
   src: string;
@@ -17,8 +18,8 @@ interface MobileScreenContent {
   id: string;
   title?: string;
   description?: string;
-  githubRepo?: URL;
-  hostedSite?: URL;
+  githubRepo?: string;
+  hostedSite?: string;
   image?: ImageData;
   backgroundColor?: string;
   component?: (props: { isActive: 'active' | '' }) => React.ReactNode;
@@ -31,11 +32,16 @@ const mobileScreenContents: MobileScreenContent[] = [
     backgroundColor: "transparent"
   },
   {
+    id: "instructions",
+    component: (props) => <MobileInstructions {...props} />,
+    backgroundColor: "transparent"
+  },
+  {
     id: "poke-weather",
     title: "Poke Weather",
     description: "A weather API with Pokémon based on weather conditions",
-    githubRepo: new URL("https://github.com/Byrdbass/Poke-Weather"),
-    hostedSite: new URL("http://lelandbyrd.com/assets/Poke-Weather/index.html"),
+    githubRepo: "https://github.com/Byrdbass/Poke-Weather",
+    hostedSite: "http://lelandbyrd.com/assets/Poke-Weather/index.html",
     image: {
       src: "http://lelandbyrd.com/images/PokeWeather.gif",
       alt: "screenshot of poke-weather app",
@@ -47,8 +53,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "repair-revolution",
     title: "Repair Revolution",
     description: "A blog to advocate for the Right to Repair! Reuse and Recycle!",
-    githubRepo: new URL("https://github.com/mrlane51/RepairRevolution"),
-    hostedSite: new URL("https://repairrevolution.herokuapp.com/"),
+    githubRepo: "https://github.com/mrlane51/RepairRevolution",
+    hostedSite: "https://repairrevolution.herokuapp.com/",
     image: {
       src: "https://lelandbyrd.com/images/Repair%20Revolution.gif",
       alt: "screenshot of Repair-Revolution app",
@@ -60,8 +66,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "decked-out",
     title: "Decked-Out",
     description: "Create an account and study flashcards of your favorite subject or quiz.",
-    githubRepo: new URL("https://github.com/clabel95/Decked-Out"),
-    hostedSite: new URL("https://decked--out.herokuapp.com"),
+    githubRepo: "https://github.com/clabel95/Decked-Out",
+    hostedSite: "https://decked--out.herokuapp.com",
     image: {
       src: "https://lelandbyrd.com/images/Decked-Out.gif",
       alt: "screenshot of Decked-Out app",
@@ -73,8 +79,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "werd-to-the-words",
     title: "Werd to The Words",
     description: "A progressive web app that functions as a basic text editor.",
-    githubRepo: new URL("https://github.com/Byrdbass/Werd-to-the-Words"),
-    hostedSite: new URL("https://werd-to-the-words.herokuapp.com/"),
+    githubRepo: "https://github.com/Byrdbass/Werd-to-the-Words",
+    hostedSite: "https://werd-to-the-words.herokuapp.com/",
     image: {
       src: "https://lelandbyrd.com/images/J.A.T.E.gif",
       alt: "screen shot of werd to the words app"
@@ -84,8 +90,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "byrdsbuddies",
     title: "ByrdsBuddies application",
     description: "A backend app that tracks users, their thoughts, and login info.",
-    githubRepo: new URL("https://github.com/Byrdbass/ByrdsBuddies"),
-    hostedSite: new URL("https://github.com/Byrdbass/ByrdsBuddies"),
+    githubRepo: "https://github.com/Byrdbass/ByrdsBuddies",
+    hostedSite: "https://github.com/Byrdbass/ByrdsBuddies",
     image: {
       src: "https://lelandbyrd.com/images/byrdsbuddies-gif.gif",
       alt: "screen shot of byrdsbuddies app",
@@ -97,8 +103,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "blog-bonanza",
     title: "Blog Bonanza",
     description: "A full stack tech blog!",
-    githubRepo: new URL("https://github.com/Byrdbass/Blog-Bonanza"),
-    hostedSite: new URL("https://blog-bonanza.herokuapp.com"),
+    githubRepo: "https://github.com/Byrdbass/Blog-Bonanza",
+    hostedSite: "https://blog-bonanza.herokuapp.com",
     image: {
       src: "http://lelandbyrd.com/images/Blog-Bonanza.gif",
       alt: "screenshot of Blog-Bonanza app",
@@ -110,8 +116,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "write-some-notes",
     title: "Write Some Notes",
     description: "A simple note-taking app.",
-    githubRepo: new URL("https://github.com/Byrdbass/note-taker"),
-    hostedSite: new URL("https://write-some-notes.herokuapp.com/"),
+    githubRepo: "https://github.com/Byrdbass/note-taker",
+    hostedSite: "https://write-some-notes.herokuapp.com/",
     image: {
       src: "https://lelandbyrd.com/images/Note%20Taker.gif",
       alt: "screenshot of Note Taker app",
@@ -123,8 +129,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "team-profile-generator",
     title: "Team Profile Generator",
     description: "A Node app to create an employee database.",
-    githubRepo: new URL("https://github.com/Byrdbass/Team-Profile-Generator"),
-    hostedSite: new URL("https://github.com/Byrdbass/Team-Profile-Generator"),
+    githubRepo: "https://github.com/Byrdbass/Team-Profile-Generator",
+    hostedSite: "https://github.com/Byrdbass/Team-Profile-Generator",
     image: {
       src: "https://lelandbyrd.com/images/team-profile-generator-gif.gif",
       alt: "screenshot of Team profile Generator app",
@@ -136,8 +142,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "markdown-muhsheeen",
     title: "MarkDown MUHsheeen",
     description: "A markdown file generator for easy GitHub deployment.",
-    githubRepo: new URL("https://github.com/Byrdbass/MarkDown-MUHsheeen"),
-    hostedSite: new URL("https://github.com/Byrdbass/MarkDown-MUHsheeen"),
+    githubRepo: "https://github.com/Byrdbass/MarkDown-MUHsheeen",
+    hostedSite: "https://github.com/Byrdbass/MarkDown-MUHsheeen",
     image: {
       src: "https://lelandbyrd.com/images/README-generator-gif.gif",
       alt: "screenshot of a readme generator",
@@ -149,8 +155,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "weather-dashboard",
     title: "Weather Dashboard",
     description: "A 5-day weather forecast based on location.",
-    githubRepo: new URL("https://github.com/Byrdbass/weather-dashboard"),
-    hostedSite: new URL("https://byrdbass.github.io/weather-dashboard/"),
+    githubRepo: "https://github.com/Byrdbass/weather-dashboard",
+    hostedSite: "https://byrdbass.github.io/weather-dashboard/",
     image: {
       src: "https://lelandbyrd.com/images/Weather-Dashboard-ScreenShot.gif",
       alt: "screenshot of Weather app",
@@ -162,8 +168,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "password-generator",
     title: "Password Generator",
     description: "An app to generate a password with alert boxes.",
-    githubRepo: new URL("https://github.com/Byrdbass/passwordGenerator"),
-    hostedSite: new URL("https://byrdbass.github.io/passwordGenerator/"),
+    githubRepo: "https://github.com/Byrdbass/passwordGenerator",
+    hostedSite: "https://byrdbass.github.io/passwordGenerator/",
     image: {
       src: "https://lelandbyrd.com/images/Password_gen_screenshot.png",
       alt: "screenshot of password Generator app",
@@ -175,8 +181,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "ecommerce-backend",
     title: "E-Commerce Back End",
     description: "Helps vendors link products to categories and other info.",
-    githubRepo: new URL("https://github.com/Byrdbass/E-commerce-Back-End"),
-    hostedSite: new URL("https://github.com/Byrdbass/E-commerce-Back-End"),
+    githubRepo: "https://github.com/Byrdbass/E-commerce-Back-End",
+    hostedSite: "https://github.com/Byrdbass/E-commerce-Back-End",
     image: {
       src: "https://lelandbyrd.com/images/e-commerce-back-end-screenshot.gif",
       alt: "screenshot of e-commerce back end app",
@@ -188,8 +194,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "bizness-trackstar",
     title: "Bizness-Trackstar",
     description: "Tracks employee databases including salaries and managers.",
-    githubRepo: new URL("https://github.com/Byrdbass/bizness-trackstar"),
-    hostedSite: new URL("https://github.com/Byrdbass/bizness-trackstar"),
+    githubRepo: "https://github.com/Byrdbass/bizness-trackstar",
+    hostedSite: "https://github.com/Byrdbass/bizness-trackstar",
     image: {
       src: "https://lelandbyrd.com/images/employee-tracker-screenshot.gif",
       alt: "screenshot of bizness-trackstar app",
@@ -201,8 +207,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "work-day-scheduler",
     title: "Work Day Scheduler",
     description: "Create and monitor a daily schedule to stay organized.",
-    githubRepo: new URL("https://github.com/Byrdbass/work-day-scheduler"),
-    hostedSite: new URL("https://byrdbass.github.io/work-day-scheduler"),
+    githubRepo: "https://github.com/Byrdbass/work-day-scheduler",
+    hostedSite: "https://byrdbass.github.io/work-day-scheduler",
     image: {
       src: "https://lelandbyrd.com/images/Work-day-scheduler-screenGif.gif",
       alt: "screenshot of work-day-scheduler app",
@@ -214,8 +220,8 @@ const mobileScreenContents: MobileScreenContent[] = [
     id: "code-quiz",
     title: "Code Quiz",
     description: "Quiz yourself on basic JavaScript and compete for high scores!",
-    githubRepo: new URL("https://github.com/Byrdbass/code-quiz"),
-    hostedSite: new URL("https://byrdbass.github.io/code-quiz/"),
+    githubRepo: "https://github.com/Byrdbass/code-quiz",
+    hostedSite: "https://byrdbass.github.io/code-quiz/",
     image: {
       src: "https://lelandbyrd.com/images/Code-Quiz-gif.gif",
       alt: "screenshot of an online JS code quiz",
@@ -305,7 +311,9 @@ export default function CodeProjectsMobile() {
                     <h2 className="phone-title">{screen.title}</h2>
                     <p className="phone-description">{screen.description}</p>
                     {screen.component ? screen.component({ isActive }) : null}
-                    <img src={screen.image?.src} alt={screen.image?.alt} width={screen.image?.width} />
+                    <a href={screen.githubRepo} target="_blank">
+                      <img src={screen.image?.src} alt={screen.image?.alt} width={screen.image?.width} />
+                    </a>
                   </div>
                 )
               }
--- src/components/mobile/mobileInstructions/MobileInstructions.tsx
+++ src/components/mobile/mobileInstructions/MobileInstructions.tsx
@@ -5,8 +5,20 @@ import '../phone.css'
 const MobileInstructions: React.FC<{isActive: 'active' | ''}> = ({isActive}): JSX.Element => {
     return(
         <div>
-            
+            <p className="phone-title">
+                Scroll to see projects 
+            </p>
+                <span className="phone-title">{"\u2B07"}</span> 
+            <p className="phone-description">
+                click the Image 🖼️ to see the Repo
+            </p>
+            <p className="phone-description">
+                If it is deployed: click the <br />
+                <span className="phone-title">🚀</span>
+            </p>
         </div>
 
     )
 }
+
+export default MobileInstructions;
